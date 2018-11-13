8c0039c49830f475f27da6f5ba4ee48be2e86605
RonaldoMPF <ronaldo.medeiros.filho@ccc.ufcg.edu.br>
2018-11-13 01:52:16 -0300

    improve install.sh

--- spool-project/install.sh
+++ spool-project/install.sh
@@ -11,12 +11,10 @@ touch "$configPath"
 echo "max_quota $monthQuota" >> "$configPath"
 echo "users_file_name users.txt" >> "$configPath"
 echo "quota_file_path .quota" >> "$configPath"
+echo "logs_file_path .logs" >> $configPath
 
 mkdir "$projectPath/.quota"
 mkdir "$projectPath/.logs"
 
 touch "$projectPath/users.txt"
 
-
-#sudo chmod 755 -R "$userPath/spool-project"
-#sudo chown root -R "$userPath/spool-project"
