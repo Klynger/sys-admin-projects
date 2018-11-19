199200f0a2520f641dc1a77c2159f005fcdbf2b4
RonaldoMPF <ronaldo.medeiros.filho@ccc.ufcg.edu.br>
2018-11-19 16:05:31 -0300

    starting usage project

---
+++ usage-project/shell.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+while [ 1 ]; do
+	
+	read entry
+	
+	if [ "$entry" == "exit" ]; then
+	       exit
+       	fi
+
+	/usr/bin/time -f "%S\t%U" -o /tmp/usage-time $entry
+	timeOutput=$(cat /tmp/usage-time)	
+	logLine="`whoami`@`date +%y/%m/%d-%H:%M:%S`@$timeOutput@\"$entry\""
+	echo $logLine >> log
+	echo "$"
+
+done
+
+
+	
