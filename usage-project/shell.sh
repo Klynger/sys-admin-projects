375157cae915f9018743009f81e053bfcce21b70
Rafael Klynger <rafael.klynger@gmail.com>
2018-11-26 15:10:19 -0300

    Fix problems when using pipe in a command

--- usage-project/shell.sh
+++ usage-project/shell.sh
@@ -4,12 +4,18 @@ while [ 1 ]; do
 	
 	echo -ne "[$(whoami)]@:$(pwd)$ "
 	read entry
-	
-	if [ "$entry" == "exit" ] || [ -z $(which $entry) ]; then
+	echo $entry > /tmp/usage-time-command
+	if [ "$entry" == "exit" ]; then
 	       exit
-       	fi
+    fi
 
-	/usr/bin/time -f "%S\t%U" -o /tmp/usage-time $entry
+	pureCommand=$(awk '{ print $1 }' /tmp/usage-time-command)
+	if [ -z $(which $pureCommand) ]; then
+		echo "You can't use this command here"
+		exit 1
+	fi
+
+	/usr/bin/time -f "%S\t%U" -o /tmp/usage-time bash -c "$entry"
 	timeOutput=$(cat /tmp/usage-time)	
 	logLine="`whoami`@`date +%y/%m/%d-%H:%M:%S`@$timeOutput@\"$entry\""
 	echo $logLine >> log
